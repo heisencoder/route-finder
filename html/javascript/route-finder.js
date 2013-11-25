9731b5a856c5dcac5c191d165f4dc1883ac4a9dd
Matt Ball <matthew.v.ball@colorado.edu>
2013-11-25 14:17:02 -0700

    added simple route finder and updated .gitignore

--- html/javascript/route-finder.js
+++ html/javascript/route-finder.js
@@ -1,7 +1,11 @@
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
 /**
  * Called when the page is loaded and performs all needed initialization.
  */
-function initialize() {
+initialize = function() {
   var mapOptions = {
     center: new google.maps.LatLng(40.006756,-105.263618),
     zoom: 13
@@ -10,4 +14,17 @@ function initialize() {
       mapOptions);
 }
 
-
+calcRoute = function() {
+  var start = document.getElementById('startaddr').value;
+  var end = document.getElementById('destaddr').value;
+  var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
