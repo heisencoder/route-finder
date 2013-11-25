1812bdb8b440bd104b87c700a82d7f73cce96044
Matt Ball <matthew.v.ball@colorado.edu>
2013-11-25 14:33:33 -0700

    initial version for finding one route

--- html/javascript/route-finder.js
+++ html/javascript/route-finder.js
@@ -6,12 +6,14 @@ var map;
  * Called when the page is loaded and performs all needed initialization.
  */
 initialize = function() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
   var mapOptions = {
     center: new google.maps.LatLng(40.006756,-105.263618),
     zoom: 13
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
+  directionsDisplay.setMap(map);
 }
 
 calcRoute = function() {
