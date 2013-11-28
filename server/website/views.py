8ca116f0f6891e70a4494cb5bb687f7ff4dcf69a
Michael Dooley <michael.dooley@gmail.com>
2013-11-27 17:19:57 -0700

    In index.html API key is now templated. Two keys provided in website/views.py

--- server/website/views.py
+++ server/website/views.py
@@ -2,14 +2,17 @@ from django.template.loader import render_to_string
 from django.http import HttpResponse
 
 
-API_KEY = "AIzaSyDusgVbcYaUqaWD6-ORU2vHgE3mqwkKamA"
+API_KEY1 = "AIzaSyDusgVbcYaUqaWD6-ORU2vHgE3mqwkKamA"
+API_KEY2 = "AIzaSyAXQPThjgrcD_VOyfoD7_VLHJWGaQ1QBuI"
+
 
 def staticFile(request, filename):
-    rendered = render_to_string(filename, {"api_key" : API_KEY})
+    rendered = render_to_string(filename)
     return HttpResponse(rendered)
 
 
 def indexFile(request):
     filename = 'index.html'
-    rendered = render_to_string(filename, {"api_key" : API_KEY})
+    context = {"api_key" : API_KEY1}
+    rendered = render_to_string(filename, context)
     return HttpResponse(rendered)
